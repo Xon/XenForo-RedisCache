0397cf3180ece3f55823d935ceb32a9c6430f892
Xon <tazipper1@hotmail.com>
2015-01-24 19:28:30 +0800

    Update example

--- config.redis-example.php
+++ config.redis-example.php
@@ -1,11 +1,11 @@
 <?php
-
-
+// setup redis caching
 $config['cache']['backend'] = 'Redis';
 $config['cache']['backendOptions'] = array(
         'server' => '127.0.0.1',
         'port' => 6379,
         );
-        
+// install Redis-aware XF Caching replacement. Will break in XF 2.0
+require(XenForo_Application::getConfigDir().'/SV/RedisCache/Installer.php');        
         
 // $redis = XenForo_Application::getCache()->getBackend()->getCredis();        
