3256e6ee38e780cbfb04e70fce469613042de8dc
Xon <tazipper1@hotmail.com>
2015-08-26 09:25:52 +0800

    Instead of deleting, adjust the expiry for invalidated styles

--- upload/library/SV/RedisCache/XenForo/Model/Style.php
+++ upload/library/SV/RedisCache/XenForo/Model/Style.php
@@ -11,17 +11,53 @@ class SV_RedisCache_XenForo_Model_Style extends XFCP_SV_RedisCache_XenForo_Model
 
     public function styleCachePurge($style_id = null)
     {
-        $cachePattern = "xfCssCache_style_";
+        $registry = $this->_getDataRegistryModel();
+        $cache = $this->_getCache(true);
+        if (!method_exists($registry, 'getCredis') && $credis = $registry->getCredis($cache))
+        {
+            return;
+        }
+
+        static $cachePrefix = null;
+        if ($cachePrefix === null)
+        {
+            $cachePrefix = $cache->getOption('cache_id_prefix');
+        }
+
+        $cachePattern = Cm_Cache_Backend_Redis::PREFIX_KEY . $cachePrefix . "xfCssCache_style_";
         if ($style_id)
         {
             $cachePattern .= $style_id . "_";
         }
         $cachePattern .= "*";
-
-        $registry = $this->_getDataRegistryModel();
-        if (method_exists($registry, 'deleteMulti'))
+        $expiry = 5*60;
+        // indicate to the redis instance would like to process X items at a time.
+        $count = 1000;
+        // find indexes matching the pattern
+        $cursor = null;
+        $keys = array();
+        while(true)
         {
-            $registry->deleteMulti($cachePattern);
+            $next_keys = $credis->scan($cursor, $pattern, $count);
+            // scan can return an empty array
+            if($next_keys)
+            {
+                $keys += $next_keys;
+            }
+            if (empty($cursor) || $next_keys === false)
+            {
+                break;
+            }
+        }
+        if ($keys)
+        {
+            // adjust TTL them, use pipe-lining
+            $credis->pipeline()->multi();
+            foreach($keys as $key)
+            {
+                $credis->ttl($key, $expiry);
+            }
+            $credis->exec();
         }
     }
 }
