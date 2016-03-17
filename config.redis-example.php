65441ac97fcd4a4b1e250957dc55e2559d47d75e
Xon <tazipper1@hotmail.com>
2016-03-17 22:54:28 +0800

    Extend example config with all config options.

--- config.redis-example.php
+++ config.redis-example.php
@@ -1,6 +1,29 @@
 <?php
 // setup redis caching
 $config['cache']['backend'] = 'Redis';
+// all keys and thier defaults
+$config['cache']['backendOptions'] = array(
+        'server' => '127.0.0.1',
+        'port' => 6379,
+        'timeout' => 2.5,
+        'persistent' : '',
+        'force_standalone' => false,
+        'connect_retries' => 1,
+        'read_timeout' => '',
+        'password' => '',
+        'database' => 0,
+        'notMatchingTags' => false,
+        'compress_tags' => 1,
+        'compress_data' => 1,
+        'lifetimelimit' => 2592000,
+        'compress_threshold' => 20480,
+        'automatic_cleaning_factor' => 0,
+        'compression_lib' => '', // dynamically select first of; snappy,lzf,l4z,gzip
+        'use_lua' => false,
+        'sunion_chunk_size' => 500,
+        'lua_max_c_stack' => 5000,
+        );
+// minimal case
 $config['cache']['backendOptions'] = array(
         'server' => '127.0.0.1',
         'port' => 6379,
