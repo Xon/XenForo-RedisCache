1636d5597ddcd7dd27b0cfd1b51264f55c38c453
Xon <tazipper1@hotmail.com>
2016-05-03 23:15:45 +0800

    Fix svg template support

--- upload/library/SV/RedisCache/SV/SVGTemplate/SvgOutput.php
+++ upload/library/SV/RedisCache/SV/SVGTemplate/SvgOutput.php
@@ -1,6 +1,6 @@
 <?php
 
-class SV_RedisCache_XenForo_SvgOutput extends XFCP_SV_RedisCache_XenForo_SvgOutput
+class SV_RedisCache_SV_SVGTemplate_SvgOutput extends XFCP_SV_RedisCache_SV_SVGTemplate_SvgOutput
 {
     public function getCacheId()
     {
@@ -8,8 +8,8 @@ class SV_RedisCache_XenForo_SvgOutput extends XFCP_SV_RedisCache_XenForo_SvgOutp
             '_style_' . $this->_styleId .
             '_d_' . $this->_inputModifiedDate .
             '_language_' . $this->_languageId .
-            '_debug_' . (XenForo_Application::debugMode() ? 'debug' : '') .
-            '_svg_' . $this->_svgRequested
-            );
+            '_debug_' . (XenForo_Application::debugMode() ? '1' : '0') .
+            '_svg_' . sha1($this->_svgRequested)
+            ;
     }
 }
