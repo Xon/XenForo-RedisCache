f8097b1a07e66fcdb4e183fcdd30f038905327be
Xon <tazipper1@hotmail.com>
2016-10-28 07:51:48 +0800

    Update Cm_Cache_Backend_Redis (bugfix)

--- upload/library/Zend/Cache/Backend/Redis.php
+++ upload/library/Zend/Cache/Backend/Redis.php
@@ -2,7 +2,7 @@
 
 require('Redis/lib/Credis/Client.php');
 //require('Redis/lib/Credis/Cluster.php');
-//require('Redis/lib/Credis/Sentinel.php');
+require('Redis/lib/Credis/Sentinel.php');
 require('Redis/Cm/Cache/Backend/Redis.php');
 class Zend_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
 {
