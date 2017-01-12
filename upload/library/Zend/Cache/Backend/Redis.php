501b1d0f41d80bc92c659001a6cf7eec42a0c257
Xon <tazipper1@hotmail.com>
2017-01-12 22:59:17 +0800

    Initial attempt at 'preferLocalSlave'

--- upload/library/Zend/Cache/Backend/Redis.php
+++ upload/library/Zend/Cache/Backend/Redis.php
@@ -6,6 +6,38 @@ require('Redis/lib/Credis/Sentinel.php');
 require('Redis/Cm/Cache/Backend/Redis.php');
 class Zend_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
 {
+    public function __construct($options = array())
+    {
+        if (!isset($options['slave-select']))
+        {
+            $options['slave-select'] = array($this, 'preferLocalSlave');
+        }
+        parent::__construct($options);
+    }
+
+    public function preferLocalSlave(array $slaves)
+    {
+        // I can't believe there isn't a better way
+        $output = shell_exec("hostname --all-ip-addresses");
+        if ($output)
+        {
+            $ips = explode(' ', $output);
+            /* @var $slave Credis_Client */
+            foreach($slaves as $slave)
+            {
+                // slave host is just an ip
+                $host = $slave->getHost();
+                if (in_array($host, $ips))
+                {
+                    return $host;
+                }
+            }
+        }
+
+        $slaveKey = array_rand($slaves, 1);
+        return $slaves[$slaveKey];
+    }
+
     public function getCompressThreshold()
     {
         return $this->_compressThreshold;
