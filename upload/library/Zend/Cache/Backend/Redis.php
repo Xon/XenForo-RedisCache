c1cfb85bb9827dc4d3436351435af49302ca61d9
Xon <tazipper1@hotmail.com>
2017-01-19 16:14:11 +0800

    Support APCu

--- upload/library/Zend/Cache/Backend/Redis.php
+++ upload/library/Zend/Cache/Backend/Redis.php
@@ -17,17 +17,39 @@ class Zend_Cache_Backend_Redis extends Cm_Cache_Backend_Redis
 
     public function preferLocalSlave(array $slaves)
     {
-        // I can't believe there isn't a better way
-        $output = shell_exec("hostname --all-ip-addresses");
-        if ($output)
+        $hasIps = false;
+        $ips = array();
+        if (function_exists('apcu_fetch'))
         {
-            $ips = array_filter(array_map('trim', (explode(' ', $output))));
+            $ips = apcu_fetch('localips', $hasIps);
+        }
+        if (!$hasIps || !is_array($ips))
+        {
+            // I can't believe there isn't a better way
+            try
+            {
+                $output = shell_exec("hostname --all-ip-addresses");
+            }
+            catch(Exception $e) { $output = ''; }
+            if ($output)
+            {
+                $ips = array_fill_keys(array_filter(array_map('trim', (explode(' ', $output)))), true);
+                if (function_exists('apcu_store'))
+                {
+                    // bit racing on the first connection, but local IPs rarely change.
+                    apcu_store('localips', $ips);
+                }
+            }
+        }
+
+        if ($ips)
+        {
             /* @var $slave Credis_Client */
             foreach($slaves as $slave)
             {
                 // slave host is just an ip
                 $host = $slave->getHost();
-                if (in_array($host, $ips))
+                if (isset($ips[$host]))
                 {
                     return $slave;
                 }
